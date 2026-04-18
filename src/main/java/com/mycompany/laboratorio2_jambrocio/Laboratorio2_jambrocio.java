f285095425941b7f01acd2c1a3ec7bf85989521d
Josue Ambrocio <Josue Ambrocio@Josue>
2026-04-18 13:26:04 -0600

    Se crean los diseños para la aplicación correspondiente

---
+++ src/main/java/com/mycompany/laboratorio2_jambrocio/Laboratorio2_jambrocio.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratorio2_jambrocio;
+
+/**
+ *
+ * @author Josue Ambrocio
+ */
+public class Laboratorio2_jambrocio {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        
+        login.setLocationRelativeTo(null);
+        login.setVisible(true);
+    }
+}
